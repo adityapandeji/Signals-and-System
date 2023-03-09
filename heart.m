254c5ed971e849828fc371d585cf24491d0f9a27
Pande Aditya <adipande901@gmail.com>
2023-03-09 23:07:13 +0530

    Create heart.m

---
+++ heart.m
@@ -0,0 +1,9 @@
+%NAME:Pande Aditya
+%Roll No: 22251
+%Date: 10 Feb 2023
+%Title:The Beginning is Beautiful
+
+t = linspace(-pi,pi, 1000);
+X =      t .* sin( pi * 1*sin(t)./t);
+Y = -abs(t) .* cos(pi * sin(t)./t);
+plot(X,Y);
